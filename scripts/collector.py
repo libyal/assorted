74cbe53787314991a87247a07fa491fd98080a9f
Joachim Metz <joachim.metz@gmail.com>
2016-03-06 09:12:55 +0100

    Worked on WSI extract script.

---
+++ scripts/collector.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""Classes to implement a Windows volume collector."""
+
+import dfvfs
+import dfwinreg
+
+from dfvfs.helpers import volume_scanner as dfvfs_volume_scanner
+from dfvfs.lib import definitions as dfvfs_definitions
+from dfvfs.path import factory as dfvfs_path_spec_factory
+from dfvfs.resolver import resolver as dfvfs_resolver
+from dfwinreg import interface as dfwinreg_interface
+from dfwinreg import regf as dfwinreg_regf
+
+
+if dfvfs.__version__ < u'20160227':
+  raise ImportWarning(u'collector.py requires dfvfs 20160227 or later.')
+
+if dfwinreg.__version__ < u'20151026':
+  raise ImportWarning(u'collector.py requires dfwinreg 20151026 or later.')
+
+
+class WindowsVolumeCollector(dfvfs_volume_scanner.WindowsVolumeScanner):
+  """Class that defines a Windows volume collector."""
+
+  def __init__(self, mediator=None):
+    """Initializes the collector object.
+
+    Args:
+      mediator: a volume scanner mediator (instance of
+                VolumeScannerMediator) or None.
+    """
+    super(WindowsVolumeCollector, self).__init__(mediator=mediator)
+    self._single_file = False
+
+  def OpenFile(self, windows_path):
+    """Opens the file specificed by the Windows path.
+
+    Args:
+      windows_path: the Windows path to the file.
+
+    Returns:
+      The file-like object (instance of dfvfs.FileIO) or None if
+      the file does not exist.
+    """
+    if not self._single_file:
+      return super(WindowsVolumeCollector, self).OpenFile(windows_path)
+
+    # TODO: check name of single file.
+    path_spec = dfvfs_path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_OS, location=self._source_path)
+    if path_spec is None:
+      return
+
+    return dfvfs_resolver.Resolver.OpenFileObject(path_spec)
+
+  def ScanForWindowsVolume(self, source_path):
+    """Scans for a Windows volume.
+
+    Args:
+      source_path: a string containing the source path.
+
+    Returns:
+      A boolean value indicating if a Windows volume was found.
+
+    Raises:
+      ScannerError: if the source path does not exists, or if the source path
+                    is not a file or directory, or if the format of or within
+                    the source file is not supported.
+    """
+    result = super(WindowsVolumeCollector, self).ScanForWindowsVolume(
+        source_path)
+    if not result:
+      return False
+
+    if self._source_type == dfvfs_definitions.SOURCE_TYPE_FILE:
+      self._single_file = True
+
+    return True
+
+
+class CollectorRegistryFileReader(dfwinreg_interface.WinRegistryFileReader):
+  """Class that defines the collector-based Windows Registry file reader."""
+
+  def __init__(self, volume_scanner):
+    """Initializes the Windows Registry file reader object.
+
+    Args:
+      volume_scanner: the Windows volume scanner (instance of
+                      WindowsVolumeCollector).
+    """
+    super(CollectorRegistryFileReader, self).__init__()
+    self._volume_scanner = volume_scanner
+
+  def Open(self, path, ascii_codepage=u'cp1252'):
+    """Opens the Windows Registry file specificed by the path.
+
+    Args:
+      path: string containing the path of the Windows Registry file. The path
+            is a Windows path relative to the root of the file system that
+            contains the specfic Windows Registry file. E.g.
+            C:\\Windows\\System32\\config\\SYSTEM
+      ascii_codepage: optional ASCII string codepage.
+
+    Returns:
+      The Windows Registry file (instance of WinRegistryFile) or None.
+    """
+    file_object = self._volume_scanner.OpenFile(path)
+    if file_object is None:
+      return
+
+    registry_file = dfwinreg_regf.REGFWinRegistryFile(
+        ascii_codepage=ascii_codepage)
+
+    try:
+      registry_file.Open(file_object)
+    except IOError:
+      file_object.close()
+      return
+
+    return registry_file
--- scripts/wsi_extract.py
+++ scripts/wsi_extract.py
@@ -8,17 +8,159 @@ import logging
 import sys
 
 import pylnk
+import pysigscan
 
+import collector
 import hexdump
 
 
 # pylint: disable=logging-format-interpolation
 
-# TODO: add dfvfs collector support.
-# TODO: extract WSI from LNK.
-# TODO: extract WSI from BagsMRU.
-# TODO: extract WSI from MRU.
-# TODO: extract WSI from JumpLists.
+class WindowsShellItemExtractor(collector.WindowsVolumeCollector):
+  """Class that defines a Windows shell item extractor."""
+
+  _SIGNATURES = [
+      (u'creg', 0, b'CREG'),
+      (u'lnk', 0, (b'\x4c\x00\x00\x00\x01\x14\x02\x00\x00\x00\x00\x00\xc0'
+                   b'\x00\x00\x00\x00\x00\x00\x46')),
+      (u'olecf', 0, b'\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1'),
+      (u'olecf_beta', 0, b'\x0e\x11\xfc\x0d\xd0\xcf\x11\x0e'),
+      (u'regf', 0, b'regf'),
+  ]
+
+  def __init__(self, debug=False, mediator=None):
+    """Initializes the Windows shell item extractor object.
+
+    Args:
+      debug: optional boolean value to indicate if debug information should
+             be printed.
+      mediator: a volume scanner mediator (instance of
+                dfvfs.VolumeScannerMediator) or None.
+    """
+    super(WindowsShellItemExtractor, self).__init__(mediator=mediator)
+    self._debug = debug
+
+  # TODO: extract WSI from LNK.
+  # TODO: extract WSI from BagsMRU.
+  # TODO: extract WSI from MRU.
+  # TODO: extract WSI from JumpLists.
+
+  def _ExtractFromLNKFile(self, output_writer, path):
+    """Extracts Windows shell item from a Windows Shortcut (LNK) file.
+
+    Args:
+      output_writer: the output writer (instance of OutputWriter).
+      path: a string containing the path of the LNK file.
+    """
+    file_object = None
+
+    try:
+      lnk_file = pylnk.file()
+      lnk_file.open_file_object(file_object)
+      lnk_file.link_target_identifier_data
+      lnk_file.close()
+    finally:
+      file_object.close()
+
+  def _GetSignature(self, file_object):
+    """Determins the if the file content contains a known signature.
+
+    Args:
+      file_object: a file-like object (instance of dfvfs.FileIO).
+
+    Returns:
+      A string containing the signature identifier or None if no known
+      signature was found.
+    """
+    scan_state = pysigscan.scan_state()
+    self._file_scanner.scan_file_object(scan_state, file_object)
+    return scan_state.scan_results
+
+  def _GetSignatureScanner(self):
+    """Retrieves a signature scanner object.
+
+    Returns:
+      A scanner object (instance of pysigscan.scanner).
+    """
+    scanner_object = pysigscan.scanner()
+    for identifier, pattern_offset, pattern in self._SIGNATURES:
+      scanner_object.add_signature(
+          identifier, pattern_offset, pattern,
+          pysigscan.signature_flags.RELATIVE_FROM_START)
+
+    return scanner_object
+
+  def ExtractWindowsShellItems(self, output_writer, path):
+    """Extracts Windows shell items.
+
+    Args:
+      output_writer: the output writer (instance of OutputWriter).
+    """
+    if self._single_file:
+      # TODO: determine file type.
+      file_object = self.OpenFile(path)
+      signatures = self._GetSignature()
+    else:
+      # TODO: recurse and find supported file types.
+
+
+class FileOutputWriter(object):
+  """Class that defines a file output writer."""
+
+  def __init__(self, output_directory):
+    """Initializes the output writer object.
+
+    Args:
+      output_directory: a string containing the path of the output directory.
+    """
+    super(FileOutputWriter, self).__init__()
+    self._output_directory = output_directory
+
+  def Close(self):
+    """Closes the output writer object."""
+    pass
+
+  def Open(self):
+    """Opens the output writer object.
+
+    Returns:
+      A boolean containing True if successful or False if not.
+    """
+    return True
+
+  def WriteShellItem(self, shell_item_data):
+    """Writes a shell item.
+
+    Args:
+      shell_item_data: a binary string containing the shell item data.
+    """
+    with open(options.output_file, 'wb') as output_file:
+      output_file.write(shell_item_data)
+
+
+class StdoutOutputWriter(object):
+  """Class that defines a stdout output writer."""
+
+  def Close(self):
+    """Closes the output writer object."""
+    pass
+
+  def Open(self):
+    """Opens the output writer object.
+
+    Returns:
+      A boolean containing True if successful or False if not.
+    """
+    return True
+
+  def WriteShellItem(self, shell_item_data):
+    """Writes a shell item.
+
+    Args:
+      shell_item_data: a binary string containing the shell item data.
+    """
+    print(hexdump.Hexdump(shell_item_data))
+
 
 def Main():
   """The main program function.
@@ -30,9 +172,14 @@ def Main():
       u'Extracts Windows Shell items from the source.'))
 
   argument_parser.add_argument(
-      u'-o', u'--output-file', u'--output_file', dest=u'output_file',
-      action=u'store', metavar=u'FILE', default=None, help=(
-          u'name of the output file to write the data to.'))
+      u'-d', u'--debug', dest=u'debug', action=u'store_true', default=False,
+      help=u'enable debug output.')
+
+  argument_parser.add_argument(
+      u'-o', u'--output-directory', u'--output_directory',
+      dest=u'output_directory', action=u'store', metavar=u'PATH',
+      default=None, help=(
+          u'path of the directory to write the output data to.'))
 
   argument_parser.add_argument(
       u'source', nargs=u'?', action=u'store', metavar=u'PATH',
@@ -51,17 +198,35 @@ def Main():
   logging.basicConfig(
       level=logging.INFO, format=u'[%(levelname)s] %(message)s')
 
-  lnk_file = pylnk.file()
-  lnk_file.open(options.source)
+  if options.output_directory:
+    if not os.path.exists(options.output_directory):
+      os.mkdir(options.output_directory)
 
-  if not options.output_file:
-    print(hexdump.Hexdump(lnk_file.link_target_identifier_data))
+    if not os.path.isdir(options.output_directory):
+      print(u'{0:s} must be a directory'.format(options.output_directory))
+      print(u'')
+      return False
 
+    output_writer = FileOutputWriter(options.output_directory)
   else:
-    with open(options.output_file, 'wb') as output_file:
-      output_file.write(lnk_file.link_target_identifier_data)
+    output_writer = StdoutOutputWriter()
 
-  lnk_file.close()
+  if not output_writer.Open():
+    print(u'Unable to open output writer.')
+    print(u'')
+    return False
+
+  # TODO: pass mediator.
+  extractor = WindowsShellItemExtractor(debug=options.debug)
+
+  if not extractor.ScanForWindowsVolume(options.source):
+    print((u'Unable to retrieve the volume with the Windows directory from: '
+           u'{0:s}.').format(options.source))
+    print(u'')
+    return False
+
+  extractor.ExtractWindowsShellItems(output_writer)
+  output_writer.Close()
 
   return True
 
