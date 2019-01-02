6c47166918051688a31bcd7a64ace3cdb1d8710c
Joachim Metz <joachim.metz@gmail.com>
2019-01-02 09:43:10 +0100

    Applied updates

--- src/ascii7decompress.c
+++ src/ascii7decompress.c
@@ -1,7 +1,7 @@
 /*
  * Decompresses 7-bit ASCII compressed data
  *
- * Copyright (C) 2008-2018, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2019, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- src/fletcher32sum.c
+++ src/fletcher32sum.c
@@ -1,7 +1,7 @@
 /*
  * Calculates a Fletcher-32 of file data
  *
- * Copyright (C) 2008-2018, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2019, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
