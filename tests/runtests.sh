b9fcd820c258c84e28c2a37d56001bca386d6ad2
Joachim Metz <joachim.metz@gmail.com>
2021-04-03 13:16:39 +0200

    Changes for CI tests

---
+++ tests/runtests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Script to run tests
+#
+# Version: 20201121
+
+make check CHECK_WITH_STDERR=1;
+RESULT=$?;
+
+if test ${RESULT} -ne 0 && test -f tests/test-suite.log;
+then
+	cat tests/test-suite.log;
+fi
+exit ${RESULT};
+
