f53238cb38e8b7a4ad22d52401097b7324dd07a6
Joachim Metz <joachim.metz@gmail.com>
2022-08-08 20:20:04 +0200

    Added OSS-Fuzz targets

---
+++ ossfuzz/bzip_fuzzer.cc
@@ -0,0 +1,50 @@
+/*
+ * OSS-Fuzz target for BZip decompress function
+ *
+ * Copyright (C) 2008-2022, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "../src/assorted_bzip.h"
+
+/* Note that some of the OSS-Fuzz engines use C++
+ */
+extern "C" {
+
+int LLVMFuzzerTestOneInput(
+     const uint8_t *data,
+     size_t size )
+{
+	uint8_t uncompressed_data[ 64 * 1024 ];
+
+	size_t uncompressed_data_size = 64 * 1024;
+
+	assorted_bzip_decompress(
+	 data,
+	 size,
+	 uncompressed_data,
+	 &uncompressed_data_size,
+	 NULL );
+
+	return( 0 );
+}
+
+} /* extern "C" */
+
